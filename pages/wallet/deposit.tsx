804baaae7c637b6183db5edb3c7da6bf36e10156
s.feli <samran.f.m@gmail.com>
2024-10-05 12:27:45 +0330

    test branch protection

--- pages/wallet/deposit.tsx
+++ pages/wallet/deposit.tsx
@@ -23,7 +23,7 @@ const Deposit : NextPage = () => {
                     <h3 className="text-xl md:text-2xl font-semibold mb-4 md:mb-8">
                         شارژ کیف پول
                     </h3>  
-                    {/* <ChargeWallet />                   */}
+                    {/* <ChargeWallet />       */}
                 </div>
             )
 

18ae48b40c32dd4435d1b120d3c97a9efc5a96af
sepid <sepide.bri@gmail.com>
2025-09-08 17:45:24 +0330

    fix(ui): make responsive for header in about-us

--- modules/shared/components/header/Navigation.tsx
+++ modules/shared/components/header/Navigation.tsx
@@ -3,30 +3,26 @@ import { useTranslation } from "next-i18next";
 import { StrapiData } from "../../types/common";
 
 type Props = {
-    items: StrapiData['menuItems']
-}
-const Navigation: React.FC<Props> = props => {
+  items: StrapiData["menuItems"];
+  mobile?: boolean;
+};
 
-    const { t } = useTranslation('common');
-    const linkClassName = 'whitespace-nowrap px-1.5 lg:px-5 pb-3 md:py-3 block transition-all duration-200 text-neutral-700 hover:text-blue-700 text-sm md:text-md';
+const Navigation: React.FC<Props> = ({ items, mobile }) => {
+  const { t } = useTranslation("common");
 
-    return (
-        <div className="overflow-auto">
-            <nav className="max-sm:mt-2 inline-flex">
+  const linkClassName = mobile
+    ? "block py-2 text-neutral-700 hover:text-blue-700 text-base"
+    : "whitespace-nowrap px-1.5 lg:px-5 pb-3 md:py-3 block transition-all duration-200 text-neutral-700 hover:text-blue-700 text-sm md:text-md";
 
-                {props.items.map(item => (
-                    <Link
-                        key={item.Text}
-                        href={item.Url || ""}
-                        className={linkClassName}
-                    >
-                        {item.Text}
-                    </Link>
-                ))}
-
-            </nav>
-        </div>
-    )
-}
+  return (
+    <nav className={mobile ? "flex flex-col space-y-2" : "inline-flex"}>
+      {items.map((item) => (
+        <Link key={item.Text} href={item.Url || ""} className={linkClassName}>
+          {item.Text}
+        </Link>
+      ))}
+    </nav>
+  );
+};
 
 export default Navigation;
